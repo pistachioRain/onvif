265ef2a83bb09b741a35114e6c6b484afc07ae5d
pistachioRain <53073604+pistachioRain@users.noreply.github.com>
2025-09-26 13:28:44 +0800

    Update types.go

--- media/types.go
+++ media/types.go
@@ -1,8 +1,8 @@
 package media
 
 import (
-	"github.com/use-go/onvif/xsd"
-	"github.com/use-go/onvif/xsd/onvif"
+	"github.com/pistachioRain/onvif/xsd"
+	"github.com/pistachioRain/onvif/xsd/onvif"
 )
 
 type Capabilities struct {

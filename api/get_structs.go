c0119ce0a9bfc2ec186cf81c6b6079ac42b37f67
pistachioRain <53073604+pistachioRain@users.noreply.github.com>
2025-09-26 13:27:11 +0800

    Update get_structs.go

--- api/get_structs.go
+++ api/get_structs.go
@@ -3,9 +3,9 @@ package api
 import (
 	"errors"
 
-	"github.com/use-go/onvif/device"
-	"github.com/use-go/onvif/media"
-	"github.com/use-go/onvif/ptz"
+	"github.com/pistachioRain/onvif/device"
+	"github.com/pistachioRain/onvif/media"
+	"github.com/pistachioRain/onvif/ptz"
 )
 
 func getPTZStructByName(name string) (interface{}, error) {

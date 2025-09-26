2582c68d828ccbd215a26ac3a7b6cc6310d6377c
pistachioRain <53073604+pistachioRain@users.noreply.github.com>
2025-09-26 13:28:20 +0800

    Update discovery_test.go

--- examples/discovery_test.go
+++ examples/discovery_test.go
@@ -10,9 +10,9 @@ import (
 	"testing"
 
 	"github.com/beevik/etree"
-	"github.com/use-go/onvif"
-	"github.com/use-go/onvif/device"
-	discover "github.com/use-go/onvif/ws-discovery"
+	"github.com/pistachioRain/onvif"
+	"github.com/pistachioRain/onvif/device"
+	discover "github.com/pistachioRain/onvif/ws-discovery"
 )
 
 func TestGetAvailableDevicesAtSpecificEthernetInterface(t *testing.T) {

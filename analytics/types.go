3863434a65eed69c9caf96ffe33c5940c88c03ed
pistachioRain <53073604+pistachioRain@users.noreply.github.com>
2025-09-26 13:26:30 +0800

    Update types.go

--- analytics/types.go
+++ analytics/types.go
@@ -1,8 +1,8 @@
 package analytics
 
 import (
-	"github.com/use-go/onvif/xsd"
-	"github.com/use-go/onvif/xsd/onvif"
+	"github.com/pistachioRain/onvif/xsd"
+	"github.com/pistachioRain/onvif/xsd/onvif"
 )
 
 type GetSupportedRules struct {
